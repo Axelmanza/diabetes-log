fa9f2aa52a6708e00d786a98a392c9dcb0132c05
Alejandro Manzanares <56071710+Axelmanza@users.noreply.github.com>
2024-11-25 16:35:12 +0100

    tabs routing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,7 @@ import {
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { FoodCardComponent } from '../components/food-card/food-card.component';
-import { FoodModel } from '../intarfaces/food.interface';
+import { FoodModel } from '../interfaces/food.interface';
 
 @Component({
   selector: 'app-tab1',
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,30 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'hcinfo',
         loadComponent: () =>
           import('../tab1/tab1.page').then((m) => m.CalculatorComponent),
       },
       {
-        path: 'tab2',
+        path: 'foods',
         loadComponent: () =>
           import('../tab2/tab2.page').then((m) => m.Tab2Page),
       },
       {
-        path: 'tab3',
+        path: 'user',
         loadComponent: () =>
           import('../tab3/tab3.page').then((m) => m.Tab3Page),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/hcinfo',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/hcinfo',
     pathMatch: 'full',
   },
 ];
